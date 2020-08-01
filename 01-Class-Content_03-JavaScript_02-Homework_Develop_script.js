b0283a7f68600a97f5f1466f72551618462e9c0c
Chris Collins <ccollins36@icloud.com>
2020-08-01 18:12:34 -0400

    added confirm

--- 01-Class-Content_03-JavaScript_02-Homework_Develop_script.js
+++ 01-Class-Content_03-JavaScript_02-Homework_Develop_script.js
@@ -1,4 +1,24 @@
 // Assignment Code
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+'P','Q','R','S','T','U','V','W','X','Y','Z'];
+var uppercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+'p','q','r','s','t','u','v','w','x','y','z'];
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+
+function userInfo(){
+var length = parseInt (prompt("How many chacters would you like your password to be"));
+if(length < 8 || length > 129){
+  alert("The password must be min 8 characters and no more than 128 chacters");
+  return;
+}
+var upper = confirm("Would you like to use uppercase letters in your password");
+var lower = confirm("Would you like to use lowercase letters in your password");
+var num = confirm(" Would you like to use numbers in your password");
+if(upper === false && lower === false && num === false){
+  alert ("At lest one type of chacters must be used");
+}
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
