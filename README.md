db6d1a3597ee8b605240c27dfa26b70c31fe491a
Chris Collins <ccollins36@icloud.com>
2020-08-26 20:56:31 -0400

    fixed bug on line 37

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # 03 JavaScript: Password Generator
 
+# Objective:
+1. Provide a way to generate a random password
+2. provide user a way to pick chacter options
+3. Provide user a way to pick the length of the password from 8 to 128 chacters
+4. provide this action with the push of a single button
 
+## Soution
+1. provide a backend soultion that used a modified random number generator 
+2. linked to provided 
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Assignment Code
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
 'P','Q','R','S','T','U','V','W','X','Y','Z'];
-var uppercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
 'p','q','r','s','t','u','v','w','x','y','z'];
 var numbers = ['1','2','3','4','5','6','7','8','9','0'];
 
 function userInfo(){
-var length = parseInt (prompt("How many chacters would you like your password to be"));
+var length = parseInt (prompt("How many character would you like your password to be"));
 if( length < 8 || length > 129){
   alert("The password must be min 8 characters and no more than 128 chacters");
   return;
@@ -15,7 +15,7 @@ var upper = confirm("Would you like to use uppercase letters in your password");
 var lower = confirm("Would you like to use lowercase letters in your password");
 var num = confirm(" Would you like to use numbers in your password");
 if(upper === false && lower === false && num === false){
-  alert ("At lest one type of chacters must be used");
+  alert ("At lest one type of characters must be used");
   return;}
   var useroptions = {
     length: length,
